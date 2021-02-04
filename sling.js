97d0f6efee69112353d6defbe0f915434a96b1b4
Bhavishyagaur <72553544+Bhavishyagaur@users.noreply.github.com>
2021-02-04 14:58:53 +0530

    Add files via upload

---
+++ sling.js
@@ -0,0 +1,25 @@
+class sling{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 1.2,
+            length:200 
+        }
+        
+        this.pointB = pointB
+        this.sling = Matter.Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    
+
+    display(){
+        
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+           line(pointA.x , pointA.y, pointB.x , pointB.y);
+                
+        
+    }
+    
+}
